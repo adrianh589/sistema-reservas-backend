683e39cfda6b448a03c3a2db7354fe0548e3124d
Adrian Hoyos <adrianh_589@hotmail.com>
2024-06-12 19:46:14 -0500

    Cambio en el dist apuntado de certificado

--- db/connection.ts
+++ db/connection.ts
@@ -21,7 +21,7 @@ const db = new Sequelize({
         ssl: {
             require: true,
             rejectUnauthorized: false,
-            ca: fs.readFileSync(__dirname + '/../certs/ca-cert.pem').toString(), // Ajusta la ruta según tu estructura de proyecto
+            ca: fs.readFileSync(__dirname + '/../certs/ca-cert.pem').toString(),
         },
     },
     host: typedDbConfig.DB_HOST,
--- dist/db/connection.js
+++ dist/db/connection.js
@@ -14,7 +14,7 @@ const db = new sequelize_1.Sequelize({
         ssl: {
             require: true,
             rejectUnauthorized: false,
-            ca: fs_1.default.readFileSync(__dirname + '/certs/ca-cert.pem').toString(), // Ajusta la ruta según tu estructura de proyecto
+            ca: fs_1.default.readFileSync(__dirname + '/../certs/ca-cert.pem').toString(),
         },
     },
     host: typedDbConfig.DB_HOST,
